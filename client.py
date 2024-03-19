242362a8f7344a2db8b19e7461f23911208f010a
ayel-mou <ayel-mou@student.1337.ma>
2024-03-19 20:35:05 +0000

    done

---
+++ client.py
@@ -0,0 +1,59 @@
+import os
+import signal
+import sys
+import time
+
+def putcolor(color):
+    if color == 'r':
+        print("\033[1;31m", end='')
+    elif color == 'g':
+        print("\033[1;32m", end='')
+    elif color == 'b':
+        print("\033[1;34m", end='')
+    else:
+        print("\033[0m", end='')
+
+def send_char(pid, char_code):
+    for i in range(8):
+        bit = (char_code >> i) & 1
+        if bit == 1:
+            os.kill(pid, signal.SIGUSR1)
+        elif bit == 0:
+            os.kill(pid, signal.SIGUSR2)
+        time.sleep(0.0005)
+        
+
+def sender(pid, msg):
+    for char in msg:
+        send_char(pid, ord(char))
+
+def signal_handler(sig, frame):
+    print("Exit.")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        putcolor('r')
+        print("Usage: python3 client.py <PID> <message>")
+        sys.exit(1)
+
+    pid = int(sys.argv[1])
+    message = sys.argv[2]
+
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        putcolor('r')
+        print(f"Process with PID {pid} does not exist.")
+        sys.exit(1)
+
+    try:
+        time.sleep(1)
+        sender(pid, message)
+        sender(pid , "\n")
+    except ProcessLookupError:
+        putcolor('r')
+        print("Error: Server process not found.")
+        sys.exit(1)
---
+++ server.py
@@ -0,0 +1,33 @@
+import os
+import signal
+import sys
+from client import putcolor
+from logo import putbanner
+def handling(signum, frame):
+    global i, c
+    if signum == signal.SIGUSR1:
+        c += 1 << i
+    elif signum ==  signal.SIGUSR2:
+        c += 0
+    i += 1
+    if i == 8:
+        print(chr(c), end = '', flush=True)
+        c = 0
+        i = 0
+
+signal.signal(signal.SIGUSR1, handling)
+signal.signal(signal.SIGUSR2, handling)
+
+def signal_handler(sig, frame):
+    putcolor('b')
+    print("\t\tBye, Good Luck")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+if __name__ == "__main__":
+    i = 0
+    c = 0
+    pid = os.getpid()
+    putbanner(pid)
+    while True:
+        signal.pause()
