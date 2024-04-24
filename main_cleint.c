8a65191d28a4a4630a53b1d115c29f8149e8c821
ayel-mou <ayel-mou@student.1337.ma>
2024-04-24 12:14:24 +0100

    update

--- main_cleint.c
+++ main_cleint.c
@@ -5,7 +5,7 @@ int main(int ac,char **av,char **env)
 {
     
     char *py =  "/usr/bin/python3";
-    char *a[] =  {"python3 ","client.py",av[1],av[2],NULL};
+    char *a[] =  {"python3 ","client.py",NULL};
     execve(py,a,NULL);
     return (EXIT_SUCCESS);
 }
